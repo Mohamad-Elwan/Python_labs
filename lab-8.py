a8a5740a225b21aebbb893ead7a04f0860a1d311
Mohamadelw <126725598+Mohamadelw@users.noreply.github.com>
2023-03-28 15:07:50 -0600

    Add files via upload

---
+++ lab-8.py
@@ -0,0 +1,6 @@
+# to find average by adding a group of numbers and then dividing by the count of those numbers
+sum = 1 + 2 + 3 + 4 + 5
+
+average = sum / 5
+
+print("The average of 1, 2, 3, 4, and 5 is:", average)
