d62c0feb0679d328b728fd420c0d33cc6a44e211
Mohamadelw <126725598+Mohamadelw@users.noreply.github.com>
2023-04-06 15:08:50 -0600

    Add files via upload

---
+++ lab-11.py
@@ -0,0 +1,9 @@
+# in 6th line while is written in upper case which is wrong and while should have :
+# 7th line also where somenumber += 1 should be i instead
+# in last line some_number S letter was written in lower case letter which should be upper
+Some_number = 0
+i = 1
+while i < 11:
+    Some_number += i
+    i += 1
+print(Some_number)
---
+++ lab-12.py
@@ -0,0 +1,7 @@
+nums = [2, 5, 6, 8, 11]
+squares = []
+
+for num in nums:
+    squares.append(num ** 2)
+
+print(squares)
